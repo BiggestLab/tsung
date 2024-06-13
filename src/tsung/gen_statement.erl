aa79f0970b4c31a3291e74a67d5d245964565197
ThiwakonPB <thiwakonmezenen@gmail.com>
2024-06-13 10:06:17 +0700

    Add functions to support testing LRS PUT requests

--- src/tsung/gen_statement.erl
+++ src/tsung/gen_statement.erl
@@ -1,11 +1,35 @@
 -module(gen_statement).
--export([generate/1, uuid/1]).
--include("statement.hrl").
+-export([generate_teacher_start_game/1, 
+        generate_student_joins_game/1, 
+        generate_question_start/1,
+        generate_student_ans/1,
+        generate_question_end/1,
+        uuid/1]).
 
-generate({_Pid, DynData}) ->
-    StatementMap = ?XAPI_STUDENT_ANS_STATEMENT,
-    RegistrationId = uuid_cus:to_string(uuid_cus:uuid1()),
+-include("student_ans_statement.hrl").
+-include("teacher_start_game_statement.hrl").
+-include("student_joins_game_statement.hrl").
+-include("question_start_statement.hrl").
+-include("question_end_statement.hrl").
 
+
+generate_teacher_start_game({_Pid, DynData}) ->
+    generate_statement(?XAPI_TEACHER_START_GAME_STATEMENT, DynData).
+
+generate_student_joins_game({_Pid, DynData}) ->
+    generate_statement(?XAPI_STUDENT_JOINS_GAME_STATEMENT, DynData).
+
+generate_question_start({_Pid, DynData}) ->
+    generate_statement(?XAPI_QUESTION_START_STATEMENT, DynData).
+
+generate_student_ans({_Pid, DynData}) ->
+    generate_statement(?XAPI_STUDENT_ANS_STATEMENT, DynData).
+
+generate_question_end({_Pid, DynData}) ->
+    generate_statement(?XAPI_QUESTION_END_STATEMENT, DynData).
+
+generate_statement(StatementMap, DynData) ->
+    RegistrationId = proplists:get_value(registration_id, DynData),
     StatementId = proplists:get_value(statement_id, DynData),
 
     % Update template id with the actual id
